c4aa39fcfac638bfa8d3250402d1edb3c302518b
Chinmay Bansal <chinmaybansalsc@outlook.com>
2023-02-13 10:32:45 -0500

    Added temperature details, area,and temperature

---
+++ src/components/TimeLocation.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function TimeLocation() {
+    return <div>
+        <div className='flex items-center justify-center my-8'>
+            <p className='text-white text-xl font-extralight'>
+                Monday, Febuary 13 2023 | Local Time: 9:51 AM
+            </p>
+        </div>
+
+        <div className='flex items-center justify-center my-3'>
+            <p className='text-white text-3xl font-medium' >
+                Fremont, CA
+            </p>
+        </div>
+    </div>
+
+
+}
+
+export default TimeLocation;
