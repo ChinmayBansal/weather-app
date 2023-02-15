7da7d3444a788c164f7041756311e7e236d6c6f2
Chinmay Bansal <chinmaybansalsc@outlook.com>
2023-02-15 10:04:56 -0500

    Accessed API

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ import Inputs from './components/Inputs';
 import TimeLocation from './components/TimeLocation';
 import TemperatureDetails from './components/TemperatureDetails';
 import Forecast from './components/Forecast';
+import getWeatherData from './Services/WeatherService';
+import getFormattedData from './Services/WeatherService';
 
 function App() {
+
+  const fetchWeather = async () => {
+    const data =  await getFormattedData({q: 'fremont'});
+    console.log(data);
+  };
+
+  fetchWeather();
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-r from-cyan-700 to to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
